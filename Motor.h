1dcef9572f6caafab3e0b6590e6c29d26ff0f778
Thatrabbit520 <1444507752@qq.com>
2026-02-05 13:02:50 +0800

    [初始化] 创建STM32工程，添加标准库基础头文件

---
+++ Motor.h
@@ -0,0 +1,23 @@
+#ifndef __MOTOR_H
+#define __MOTOR_H
+
+#include "stm32f10x.h"
+
+#define IN1_PIN    GPIO_Pin_5
+#define IN1_PORT   GPIOB
+#define IN2_PIN    GPIO_Pin_6
+#define IN2_PORT   GPIOB
+#define IN3_PIN    GPIO_Pin_7
+#define IN3_PORT   GPIOB
+#define IN4_PIN    GPIO_Pin_8
+#define IN4_PORT   GPIOB
+
+// PWM duty 
+#define PWM_DUTY   400
+
+//º¯Êý
+void Motor_GPIO_Init(void);
+void Motor_PWM_Init(void);
+void Motor_Forward(void);
+
+#endif
